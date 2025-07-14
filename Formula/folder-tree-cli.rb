963e0bf536530c77bfa7246cdfc18ad06062e359
Raymon Epping <your@email.com>
2025-07-14 17:19:18 +0200

    14/07/2025 - Updated configuration and fixed bugs

--- Formula/folder-tree-cli.rb
+++ Formula/folder-tree-cli.rb
@@ -2,7 +2,7 @@ class FolderTreeCli < Formula
   desc "CLI toolkit for visualizing folder structures with markdown reports"
   homepage "https://github.com/raymonepping/folder_tree_cli"
   url "https://github.com/raymonepping/homebrew-folder-tree-cli/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "" 
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed" 
   license "MIT"
   version "1.0.0"
 
